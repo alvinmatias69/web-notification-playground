a0184587df813b602d5e51adf27b493aa8246995
Matias Alvin <alvinmatias69@gmail.com>
2019-08-21 19:04:58 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,68 @@
+function requestNotification() {
+    Notification.requestPermission().then(status => {
+        setNotificationStatus(status);
+    });
+}
+
+function setNotificationStatus(status) {
+    const statusEL = document.getElementById(("NotifStatus"));
+    statusEL.innerText = status;
+
+    let color;
+    if (status == "granted") {
+        color = "green-text";
+    } else if (status == "default") {
+        color = "orange-text";
+    } else {
+        color = "red-text";
+    }
+    statusEL.classList = [color];
+
+    const button = document.getElementById("NotificationSender");
+    button.disabled = status !== "granted";
+}
+
+function sendNotification() {
+    const userOptions = getOptions();
+
+    if (checkOptions("WebWorker")) {
+        navigator.serviceWorker.ready.then(reg => {
+            reg.showNotification("hello there", userOptions);
+        });
+    } else {
+        const notification = new Notification("hello there", userOptions);
+    }
+}
+
+function checkNotificationCapability() {
+    if (!("Notification" in window)) {
+        setNotificationStatus("Browser does not support notification");
+        disableButtons();
+    } else {
+        setNotificationStatus(Notification.permission);
+        checkServiceWorkerCapability();
+    }
+}
+
+function disableButtons() {
+    const buttons = document.getElementsByTagName("button");
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].disabled = true;
+    }
+}
+
+function checkServiceWorkerCapability() {
+    if (!("serviceWorker" in navigator)) {
+        const checkbox = document.getElementById("WebWorker");
+        checkbox.disabled = true;
+
+        const warning = createWarning("WebWorker");
+
+        const wrapper = document.getElementById("WebWorkerWrapper");
+        wrapper.appendChild(warning);
+    } else {
+        navigator.serviceWorker.register('./sw.js');
+    }
+}
+
+checkNotificationCapability();
---
+++ options.js
@@ -0,0 +1,129 @@
+const actions = [
+    {
+        action: "pasta-action",
+        title: "Pasta",
+        icon: "./assets/pasta.svg"
+    },
+    {
+        action: "map-action",
+        title: "Map",
+        icon: "./assets/map-marker-outline.svg"
+    }
+];
+
+const options = {
+    body: {
+        value: "General Kenobi, you're a bold one",
+        description: "Body"
+    },
+    icon: {
+        value: "./assets/pasta.svg",
+        description: "Icon"
+    },
+    image: {
+        value: "./assets/proxy.duckduckgo.com.jpg",
+        description: "Image"
+    },
+    badge: {
+        value: "./assets/police-badge-outline.svg",
+        description: "Badge"
+    },
+    sound: {
+        value: "./assets/481138__erokia__msfxp2-420.wav",
+        description: "Sound"
+    },
+    requireInteraction: {
+        value: true,
+        description: "Persist"
+    },
+    actions: {
+        value: actions,
+        description: "Actions"
+    },
+    tag: {
+        value: "tag-heuer",
+        description: "Merge"
+    }
+};
+
+function getOptions() {
+    const userOptions = {};
+
+    for (option in options) {
+        if (checkOptions(option)) {
+            userOptions[option] = options[option].value;
+        }
+    }
+
+    return userOptions;
+}
+
+function checkOptions(key) {
+    const element = document.getElementById(key);
+    return element.checked;
+}
+
+function generateOptions() {
+    const form = document.getElementById("form");
+
+    for (option in options) {
+        form.appendChild(createOption(option));
+    }
+};
+
+function createOption(key) {
+    const div = document.createElement("div");
+    div.classList.add("row");
+    div.classList.add("valign-wrapper");
+
+    div.appendChild(createCheckbox(key));
+    div.appendChild(createDescription(options[key].description));
+    div.appendChild(createWarning(key));
+    return div;
+}
+
+function createCheckbox(key) {
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.id = key;
+    checkbox.disabled = !(key in Notification.prototype);
+
+    const span = document.createElement("span");
+    span.classList.add("lever");
+
+    const label = document.createElement("label");
+    label.appendChild(checkbox);
+    label.appendChild(span);
+
+    const div = document.createElement("div");
+    div.classList.add("switch");
+    div.appendChild(label);
+
+
+    return div;
+};
+
+function createDescription(description) {
+    const text = document.createTextNode(description);
+
+    const span = document.createElement("span");
+    span.appendChild(text);
+
+    return span;
+}
+
+function createWarning(key) {
+    const span = document.createElement("span");
+
+    if (!(key in Notification.prototype)) {
+        const text = document.createTextNode(`(your browser does not support ${key})`);
+        span.appendChild(text);
+
+        span.classList.add("red-text");
+        span.style.cssText = "padding-left: 5px;";
+    }
+
+    return span;
+}
+
+generateOptions();
